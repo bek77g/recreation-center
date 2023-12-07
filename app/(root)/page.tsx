bb49ad9dafdced9678fd7d916836b2c048caa07e
bek77g <d.zheenkul@gmail.com>
2023-12-07 16:09:43 +0600

    add: shadcn and test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <h1>Recreation center</h1>
+      <Button>Recreation center</Button>
     </main>
   );
 }
