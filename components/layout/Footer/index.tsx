17610c33808f5d1d1c30cc0b77e98f86adb832d1
Beknur Zheenkulov <d.zheenkul@gmail.com>
2024-05-13 00:36:12 +0600

    add: init footer

--- components/layout/Footer/index.tsx
+++ components/layout/Footer/index.tsx
@@ -1,7 +1,8 @@
 import { Logo } from '@/components/ui';
 import { ROUTES } from '@/constants/links.constants';
-import { Link } from 'lucide-react';
+import { Facebook, Instagram } from 'lucide-react';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 interface IFooter {}
 
@@ -24,25 +25,80 @@ const navigation: { title: string; href: string }[] = [
 	},
 ];
 
+const contacts: { value: string; type: 'tel' | 'mailto' }[] = [
+	{
+		value: '+996 999-99-99-99',
+		type: 'tel',
+	},
+	{
+		value: '+996 505-99-99-99',
+		type: 'tel',
+	},
+	{
+		value: '+996 312-99-99-99',
+		type: 'tel',
+	},
+	{
+		value: 'wXQp3@example.com',
+		type: 'mailto',
+	},
+];
+
+const socials: { icon: any; href: string; label: string }[] = [
+	{
+		icon: Facebook,
+		href: 'https://facebook.com',
+		label: '/name.kg',
+	},
+	{
+		icon: Instagram,
+		href: 'https://instagram.com',
+		label: 'name.kg',
+	},
+];
+
 export function Footer({}: IFooter) {
 	const t = useTranslations();
 
 	return (
 		<footer className='container'>
-			<div className='grid grid-cols-4 justify-center'>
+			<div className='grid grid-cols-4 justify-center gap-16'>
 				<div>
 					<Logo />
 					<ul>
 						{navigation.map(item => (
-							<li key={item.name} className='font-semibold text-center text-4'>
+							<li key={item.name}>
 								<Link href={item.href}>{t(item.title)}</Link>
 							</li>
 						))}
 					</ul>
 				</div>
-				<div></div>
-				<div></div>
-				<div></div>
+				<div>
+					<h3 className='font-[var(--second-family)] text-6'>
+						{t('Footer.address.title')}
+					</h3>
+					<p className='text-4 font-semibold'>{t('Footer.address.text')}</p>
+				</div>
+				<div>
+					<h3>{t('Footer.contacts.title')}</h3>
+					<ul>
+						{contacts.map(item => (
+							<li key={item.value}>
+								<a href={`${item.type}:${item.value}`}>{item.value}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<h3>{t('Footer.socials.title')}</h3>
+					<ul>
+						{socials.map(social => (
+							<li key={social.href}>
+								<a href={social.href}>{social.label}</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</footer>
 	);
