8ef72a2c6480386e76040c2005b1f9d7362260fb
Beknur Zheenkulov <d.zheenkul@gmail.com>
2024-05-15 01:57:19 +0600

    add: base template of intro section

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -17,7 +17,7 @@ export function Header({}: IHeader) {
 
 	const isTablet = windowWidth <= 796;
 	const navClassName = isTablet
-		? `absolute flex flex-col justify-center gap-12 text-white items-center top-0 ${
+		? `absolute flex flex-col justify-center gap-12 items-center top-0 ${
 				isCollapsed ? 'left-[200%]' : 'left-0'
 		  }  w-full h-dvh bg-[#02345a98]`
 		: '';
@@ -46,7 +46,7 @@ export function Header({}: IHeader) {
 	];
 
 	return (
-		<header className='relative pt-4'>
+		<header className='fixed top-0 left-0 right-0 py-4 text-white'>
 			<div className='container flex gap-10 whitespace-nowrap justify-between items-center [font-size:_clamp(14px,2vw,18px)]'>
 				<div className='relative z-10'>
 					<Logo />
--- components/sections/intro/index.tsx
+++ components/sections/intro/index.tsx
@@ -1,3 +1,4 @@
+import { Form, Input } from 'antd';
 import { useTranslations } from 'next-intl';
 
 export function IntroSection() {
@@ -5,8 +6,17 @@ export function IntroSection() {
 
 	return (
 		<section className='h-screen w-full bg-[#02345a98]'>
-			<div className='container'>
-				<h2>{t('title')}</h2>
+			<div className='container h-full flex flex-col justify-center items-center'>
+				<div className='flex flex-col items-center gap-6'>
+					<h2 className='text-white font-black uppercase text-center [font-family:_var(--font3)] [font-size:_clamp(30px,5vw,85px)]'>
+						{t('title')}
+					</h2>
+					<Form className='flex gap-4 w-full justify-center'>
+						<Input size='large' className='w-full max-w-[250px]' />
+						<Input size='large' className='w-full max-w-[354px]' />
+						<Input size='large' className='w-full max-w-[354px]' />
+					</Form>
+				</div>
 			</div>
 		</section>
 	);
