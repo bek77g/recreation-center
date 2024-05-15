415aee9193f18dc2cc4a74a916f905e317b6d753
bek77g <d.zheenkul@gmail.com>
2024-05-15 13:54:51 +0600

    add: custom link

---
+++ components/shared/AnchorLink/index.tsx
@@ -0,0 +1,21 @@
+import { MoveRight } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+
+export function AnchorLink({
+	children,
+	...props
+}: React.LinkHTMLAttributes<HTMLLinkElement>) {
+	return (
+		<Link
+			{...props}
+			className={`transition-transform duration-300 flex items-center gap-1 [font-family:_var(--second-family)] font-bold uppercase text-black group ${props.className}`}>
+			{children}{' '}
+			<MoveRight
+				className='transition-transform duration-300 group-hover:translate-x-1'
+				strokeWidth={1}
+				absoluteStrokeWidth
+			/>
+		</Link>
+	);
+}
