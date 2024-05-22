75b3ccca3e534c34ed7d179073a6961eac0ec26c
bek77g <d.zheenkul@gmail.com>
2024-05-22 11:12:02 +0600

    init: api instance

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_TOKEN_NAME: process.env.NEXT_PUBLIC_TOKEN_NAME,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
