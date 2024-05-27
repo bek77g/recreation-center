35a8c29ba4e2379c17ad30a5c5309adac9ca4a85
bek77g <d.zheenkul@gmail.com>
2024-05-27 16:38:01 +0600

    fix: map integrade

--- components/shared/Map/index.tsx
+++ components/shared/Map/index.tsx
@@ -1,13 +1,15 @@
 export function Map() {
   return (
-    <div className="container">
+    <div className="py-24">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1596447.9589854234!2d76.97633317358806!3d42.46903377378394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1716305428820!5m2!1sru!2skg"
-        style="border:0;"
-        allowfullscreen=""
+        src="https://yandex.ru/map-widget/v1/?ll=77.569310%2C42.441627&z=9.71"
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
+        width="100%"
+        height="500px"
+        frameborder="1"
+        allowfullscreen="true"
+        style={{ position: 'relative' }}
+      />
     </div>
   );
 }
