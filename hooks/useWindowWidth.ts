ae4a31f2fdb8236743dd92bba5c4333b4727f8a4
bek77g <d.zheenkul@gmail.com>
2024-07-03 17:20:26 +0600

    fix: use servver prefix

--- hooks/useWindowWidth.ts
+++ hooks/useWindowWidth.ts
@@ -2,14 +2,14 @@
 import { useEffect, useState } from 'react';
 
 export const useWindowWidth = (): number => {
-	const [width, setWidth] = useState();
+  const [width, setWidth] = useState();
 
-	useEffect(() => {
-		const handleResize = () => setWidth(window.innerWidth);
-		handleResize();
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-	return width;
+  return width;
 };
